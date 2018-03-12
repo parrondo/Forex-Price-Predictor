bf0c4cd715cf0312bebc15ab7a5a0f4966b77f49
Logan Liebsch <loganliebsch@utexas.edu>
2018-03-12 09:43:36 -0500

    getDataCSV can now get OHLCV data

--- getDataCSV.py
+++ getDataCSV.py
@@ -15,11 +15,14 @@ if (environment == "live"):
 	account = config['liveAccount']
 	access_token = config['liveToken']
 
-def getDataCSV(pair, granularity, numDataPoints):
+def getDataCSV(pair, granularity, numDataPoints, OHLCV, time):
 	oanda = oandapy.API(environment=environment, access_token=access_token)
 
 	end = datetime.utcnow()
-	file = open(pair+'_'+granularity+'_'+str(numDataPoints)+'.csv', 'w')
+	if OHLCV:
+		file = open(pair+'_'+granularity+'_'+str(numDataPoints)+'_OHLCV.csv', 'w')
+	else:
+		file = open(pair+'_'+granularity+'_'+str(numDataPoints)+'.csv', 'w')
 		
 	#can get max 5k datapoints in 1 call to the API
 	count = 5000
@@ -36,13 +39,33 @@ def getDataCSV(pair, granularity, numDataPoints):
 		numDataPoints -= count
 	
 	for candle in reversed(data):
-		file.write(str(candle["time"])+','+str(candle["closeMid"]) + '\n')
+		if not OHLCV:
+			if not time:
+				file.write(str(candle['closeMid']) + '\n')
+			else:
+				file.write(str(candle['time'])+','+str(candle['closeMid']) + '\n')
+		else:
+			if not time:
+				file.write(str(candle['openMid']) +',' + str(candle['highMid'])+','+str(candle['lowMid'])+','+str(candle['closeMid'])+','+str(candle['volume'])+ '\n')
+			else:
+				file.write(str(candle['time']) +','+ str(candle['openMid']) +',' + str(candle['highMid'])+','+str(candle['lowMid'])+','+str(candle['closeMid'])+','+str(candle['volume'])+ '\n')
+			
 	
 def main():
 	pair = raw_input("Enter pair: ")
 	granularity = raw_input("Enter granularity: ")
 	numDataPoints = raw_input("Enter number of datapoints: ")
-	getDataCSV(pair, granularity, int(numDataPoints))
+	OHLCV = raw_input("OHLCV? (y/n): ")
+	if (OHLCV == 'y'):
+		OHLCV = True
+	else:
+		OHLCV = False
+	time = raw_input("Time? (y/n): ")
+	if (time == 'y'):
+		time = True
+	else:
+		time = False
+	getDataCSV(pair, granularity, int(numDataPoints), OHLCV, time)
 
 		
 if __name__ == "__main__":
